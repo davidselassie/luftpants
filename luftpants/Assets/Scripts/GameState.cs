a9e2342816ae387f3aad2af11ea16f75673a841d
Kyle Dobitz <kyledobitz@gmail.com>
2015-01-24 12:24:28 -0800

    Fixed Game State. Now has Credits

--- luftpants/Assets/Scripts/GameState.cs
+++ luftpants/Assets/Scripts/GameState.cs
@@ -1,37 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameState : MonoBehaviour {
+public class GameState : MonoBehaviour
+{
+    public GameObject Logo;
+    public GameObject Instructions;
+    public GameObject Credits;
 
-	public Phases CurrentPhase;
+    public Phases CurrentPhase;
+    public bool PlayIsFinished{
+        get{
+            return Time.time > 15f;
+        }
+    }
+    public float LogoTime = 5f;
+    public float InstructionTime = 5f;
 
-	public float LogoTime = 5f;
+    private float InstructionChangeTime;
 
-	// Use this for initialization
-	void Start () {
-		CurrentPhase = Phases.LOGO;
-		GameObject.Find ("Logo").SetActive (true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		switch (CurrentPhase) {
-			case Phases.LOGO:
-			Debug.Log ("CurrentPhase:" + CurrentPhase);
-			Debug.Log ("Time.time:" + Time.time);
-			if(Time.time > LogoTime && Input.anyKeyDown){
-				Debug.Log ("Switching to " + Phases.INSTRUCTIONS.ToString());
-				GameObject.Find ("Logo").SetActive (false);
-				GameObject.Find ("Instructions").SetActive (true);
-				CurrentPhase = Phases.INSTRUCTIONS;
-			}	
-			break;
-		}
+    // Use this for initialization
+    void Start ()
+    {
+        CurrentPhase = Phases.LOGO;
+        Logo.SetActive(true);
+        Instructions.SetActive (false);
+        Credits.SetActive (false);
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        switch (CurrentPhase) {
+        case Phases.LOGO:
+            if (Time.time > LogoTime && Input.anyKeyDown) {
+                CurrentPhase = Phases.INSTRUCTIONS;
+                Logo.SetActive (false);
+                InstructionChangeTime = Time.time + InstructionTime;
+                Instructions.SetActive (true);
+            }   
+            break;
+        case Phases.INSTRUCTIONS:
+            if (Time.time > InstructionChangeTime && Input.anyKeyDown) {
+                CurrentPhase = Phases.PLAY;
+                Instructions.SetActive(false);
+            }
+            break;
+        case Phases.PLAY:
+            if(PlayIsFinished){
+                CurrentPhase = Phases.CREDITS;
+                Credits.SetActive(true);
+            }
+            break;
+        }
+    }
 
-	}
-
-	
-	public enum Phases{
-		LOGO,INSTRUCTIONS,PLAY,CREDITS
-	}
+    
+    public enum Phases
+    {
+        LOGO,
+        INSTRUCTIONS,
+        PLAY,
+        CREDITS
+    }
 }
