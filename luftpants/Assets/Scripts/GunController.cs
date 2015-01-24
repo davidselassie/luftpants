cc49fc5f3fc913943ba09e6ce9d276a9d5ffb4f4
Kyle Dobitz <kyledobitz@gmail.com>
2015-01-23 21:51:01 -0800

    Added Graybox, GunController
    Yay

---
+++ luftpants/Assets/Scripts/GunController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+    public float SpawnPeriod = 0.5f;
+    public GameObject seed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        seed.SetActive (false);
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        GameObject clone = (GameObject) Instantiate (seed);
+        clone.SetActive (true);
+        clone.transform.position = transform.position;
+        clone.transform.forward = transform.forward;
+    }
+}
