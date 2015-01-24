8fba5d324694df52c3fbfedbd0baf4ed631ba645
Kyle Dobitz <kyledobitz@gmail.com>
2015-01-24 09:28:11 -0800

    MadManGunner shoots Gun

--- luftpants/Assets/Scripts/GunController.cs
+++ luftpants/Assets/Scripts/GunController.cs
@@ -1,23 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gun : MonoBehaviour
+public class GunController : MonoBehaviour
 {
     public float SpawnPeriod = 0.5f;
-    public GameObject seed;
+    public GameObject Seed;
+    public float BulletSpeed = 3.0f;
 
     // Use this for initialization
     void Start ()
     {
-        seed.SetActive (false);
+        Seed.SetActive (false);
     }
     
     // Update is called once per frame
-    void Update ()
+    public void Fire ()
     {
-        GameObject clone = (GameObject) Instantiate (seed);
+        Debug.Log ("FIRE!");
+        GameObject clone = (GameObject) Instantiate (Seed);
         clone.SetActive (true);
         clone.transform.position = transform.position;
         clone.transform.forward = transform.forward;
+        clone.rigidbody.velocity = BulletSpeed * clone.transform.forward;
     }
 }
