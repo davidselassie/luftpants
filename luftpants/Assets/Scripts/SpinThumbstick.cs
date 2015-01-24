f6c615594333f5e941f232fd1cfabed0cee03982
nathanhinchey <devnph@gmail.com>
2015-01-24 01:43:56 -0800

    started working on thumbstick spinning capabilities

---
+++ luftpants/Assets/Scripts/SpinThumbstick.cs
@@ -0,0 +1,33 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class SpinThumbstick : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//
+//    int StickSpin(float oldX, float newX, float oldY, float newY){
+//        //returns 1 for clockwise, -1 for counterclockwise, 0 for neither
+//        if (newX > oldX){
+//            if (newY > oldY)
+//                return -1;
+//            else if (newY < oldY)
+//                return 1;
+//        }else if (newX < oldX){
+//            if (newY > oldY)
+//                return -1;
+//            else if (newY < oldY)
+//                return 1;
+//        }
+//        else
+//            return 0;
+//    }
+//	
+//	// Update is called once per frame
+//	void Update () {
+//
+//	
+//	}
+//}
