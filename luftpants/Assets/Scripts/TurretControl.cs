6a13a072e14a30af9d344ba3aa23f202edceb2df
nathanhinchey <devnph@gmail.com>
2015-01-24 09:59:47 -0800

    collab scene

--- luftpants/Assets/Scripts/TurretControl.cs
+++ luftpants/Assets/Scripts/TurretControl.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class TurretControl : MonoBehaviour {
-    public string player = "P1 ";
+    public string player = "P2 ";
     private float verticalInput;
+    private float horizontalInput;
     private bool fireControl = false;
     public GameObject projectile;
     public Vector3 launchPoint;
@@ -19,8 +20,6 @@ public class TurretControl : MonoBehaviour {
         verticalInput = Input.GetAxis(player + "Vertical");
 
         rigidbody.AddTorque(verticalInput * Vector3.up);
-        if (fireControl)
-            Instantiate(projectile,transform.position + launchPoint,this.transform.rotation);
 	
 	}
 }
