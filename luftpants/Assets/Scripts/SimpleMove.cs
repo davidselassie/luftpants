7154767c62480af1e2abbc63b778d3a0250a1ccd
nathanhinchey <devnph@gmail.com>
2015-01-23 22:41:43 -0800

    Made SimpleMove script
    SimpleMove gives pretty simple movement controls, where thumbstick left
    and right add torque & A and B buttons add forward and reverse thrust

---
+++ luftpants/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SimpleMove : MonoBehaviour {
+
+    public string player = "P1 ";
+
+    public float maxSpeed = 10f;
+    public float burstSpeed = 5f;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalInput = Input.GetAxis(player + "Horizontal");
+        if(Input.GetButton(player + "A"))
+            verticalInput = burstSpeed;
+        else if (Input.GetButton(player + "B"))
+            verticalInput = -1f * burstSpeed;
+        else
+            verticalInput = 0f;
+
+
+        rigidbody.AddTorque(horizontalInput * Vector3.up);
+
+        rigidbody.AddForce(verticalInput * transform.forward);
+	}
+}
