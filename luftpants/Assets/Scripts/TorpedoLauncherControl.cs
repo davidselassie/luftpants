49c1eb29607c15eec0a2fb76114801f34951768f
David Selassie <selassid@gmail.com>
2015-01-25 14:02:37 -0800

    Makes torpedo blow up on double press.

--- luftpants/Assets/Scripts/TorpedoLauncherControl.cs
+++ luftpants/Assets/Scripts/TorpedoLauncherControl.cs
@@ -8,14 +8,20 @@ public class TorpedoLauncherControl : APlayerControlledComponent {
 	public float reloadSeconds = 1.0f;
 	
 	protected float lastFireTime;
+	protected GameObject lastTorpedo;
 	
 	void Start () {
 		this.lastFireTime = Time.time - this.reloadSeconds;
 	}
 	
 	void FixedUpdate () {
-		if (GetButton("A")) {
-			Fire();
+		if (GetButton ("A")) {
+			if (this.lastTorpedo == null) {
+				Fire();
+			} else {
+				Destroy (this.lastTorpedo);
+				this.lastTorpedo = null;
+			}
 		}
 	}
 	
@@ -25,9 +31,9 @@ public class TorpedoLauncherControl : APlayerControlledComponent {
 	
 	public void Fire () {
 		if (CanFire ()) {
-			GameObject newTorpedo = (GameObject) Instantiate(this.torpedoPrefab, muzzle.position, muzzle.rotation);
-			PilotedTorpedo pilot = newTorpedo.GetComponent<PilotedTorpedo>();
-			newTorpedo.rigidbody.velocity = newTorpedo.transform.rotation * Vector3.forward * pilot.speed;
+			this.lastTorpedo = (GameObject) Instantiate(this.torpedoPrefab, muzzle.position, muzzle.rotation);
+			PilotedTorpedo pilot = this.lastTorpedo.GetComponent<PilotedTorpedo>();
+			this.lastTorpedo.rigidbody.velocity = this.lastTorpedo.transform.rotation * Vector3.forward * pilot.thrustSpeed;
 			pilot.Player = this.Player;
 			
             this.lastFireTime = Time.time;
