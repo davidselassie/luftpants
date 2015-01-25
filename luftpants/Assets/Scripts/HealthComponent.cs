5b9484c1632d6e0a50d256b7a7f595cfdb686a7e
nathanhinchey <devnph@gmail.com>
2015-01-25 11:44:17 -0800

    Updated Health Component

--- luftpants/Assets/Scripts/HealthComponent.cs
+++ luftpants/Assets/Scripts/HealthComponent.cs
@@ -5,26 +5,46 @@ public class HealthComponent : MonoBehaviour {
     public float health = 100.0f;
     public float MaxHealth = 100.0f;
     public float MaxEmissionRate = 5f;
-	public ParticleSystem[] smokeSystem;
 	public bool immortal = false;
+	public bool useThreeParticleTypes = false;
+	public ParticleSystem lowDamage;
+	public float lowDamageEmissionRate = 5f;
+	public ParticleSystem medDamage;
+	public float medDamageEmissionRate = 5f;
+	public ParticleSystem highDamage;
+	public float highDamageEmissionRate = 5f;
+
+	private ParticleSystem smokeSystem;
 
 	// Use this for initialization
 	void Start () {
         health = MaxHealth;
-		if (smokeSystem == null)
-			smokeSystem = GetComponents<ParticleSystem> ();
+		if (useThreeParticleTypes == false){
+			smokeSystem = GetComponent<ParticleSystem> ();
+		} else{
+			lowDamage.emissionRate = 0f;
+			medDamage.emissionRate = 0f;
+			highDamage.emissionRate = 0f;
+		}
     }
     
     void FixedUpdate () {
         if (health <= 0.0f && immortal == false) {
             Destroy(gameObject);
         }
+		if (useThreeParticleTypes){
+			if ((MaxHealth - health) / MaxHealth > 0.25f)
+				lowDamage.emissionRate = lowDamageEmissionRate;
+			if ((MaxHealth - health) / MaxHealth > 0.50f)
+				medDamage.emissionRate = medDamageEmissionRate;
+			if ((MaxHealth - health) / MaxHealth > 0.75f)
+				highDamage.emissionRate = highDamageEmissionRate;
 
-		for (int i = 0; i < smokeSystem.Length; i++){
+		}else{
 			if (health >= MaxHealth)
-        		smokeSystem[i].emissionRate = ((MaxHealth - health) / MaxHealth) * MaxEmissionRate;
+				smokeSystem.emissionRate = ((MaxHealth - health) / MaxHealth) * MaxEmissionRate;
 			else
-				smokeSystem[i].emissionRate = MaxEmissionRate;
+				smokeSystem.emissionRate = MaxEmissionRate;
 		}
 	}
 }
