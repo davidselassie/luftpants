c9ec351d6f262a515016ec9802fcb0c52b3df17f
nathanhinchey <devnph@gmail.com>
2015-01-25 09:58:54 -0800

    spire spins!

---
+++ luftpants/Assets/Scripts/SpireSpin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpireSpin : MonoBehaviour {
+
+	public float spinSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddTorque(Vector3.up * spinSpeed);
+
+	}
+
+
+}
