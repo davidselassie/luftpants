a3cfcaf9f87b998d39ab04d5e4a74f7b2190f2cc
Kyle Dobitz <kyledobitz@gmail.com>
2015-01-24 16:29:49 -0800

    Added Explosion sound

--- luftpants/Assets/Scripts/BulletController.cs
+++ luftpants/Assets/Scripts/BulletController.cs
@@ -8,7 +8,7 @@ public class BulletController : MonoBehaviour {
     public void Impact(){
         Debug.Log ("Impact!");
         MeshRenderer meshRenderer = GetComponent<MeshRenderer> ();
-        //meshRenderer.enabled = false;
+        meshRenderer.enabled = false;
         foreach (ParticleSystem particleSystem in ImpactParticles) {
             ParticleSystem impactParticles = (ParticleSystem) Instantiate(particleSystem,transform.position,transform.rotation);
             impactParticles.enableEmission = true;
