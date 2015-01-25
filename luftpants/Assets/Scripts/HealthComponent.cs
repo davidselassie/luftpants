ea541fe583693b8e29841c2e4cfe8cf7991d74d1
nathanhinchey <devnph@gmail.com>
2015-01-25 10:28:45 -0800

    updated smokeSystem in healthComponent

--- luftpants/Assets/Scripts/HealthComponent.cs
+++ luftpants/Assets/Scripts/HealthComponent.cs
@@ -5,21 +5,26 @@ public class HealthComponent : MonoBehaviour {
     public float health = 100.0f;
     public float MaxHealth = 100.0f;
     public float MaxEmissionRate = 5f;
-	//public ParticleSystem smokeSystem;
+	public ParticleSystem[] smokeSystem;
+	public bool immortal = false;
 
 	// Use this for initialization
 	void Start () {
         health = MaxHealth;
-	}
-
-	void FixedUpdate () {
-        if (health <= 0.0f) {
+		if (smokeSystem == null)
+			smokeSystem = GetComponents<ParticleSystem> ();
+    }
+    
+    void FixedUpdate () {
+        if (health <= 0.0f && immortal == false) {
             Destroy(gameObject);
         }
-        //smokeSystem = smokeSystem.GetComponent<ParticleSystem> ();
-        //smokeSystem.emissionRate = ((MaxHealth - health) / MaxHealth) * MaxEmissionRate;
-	}
 
-    void Update(){
-    }
+		for (int i = 0; i < smokeSystem.Length; i++){
+			if (health >= MaxHealth)
+        		smokeSystem[i].emissionRate = ((MaxHealth - health) / MaxHealth) * MaxEmissionRate;
+			else
+				smokeSystem[i].emissionRate = MaxEmissionRate;
+		}
+	}
 }
